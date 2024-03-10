9b24d7f9ad6ead8185e197904ca868c1360722e1
hornley <145942096+hornley@users.noreply.github.com>
2024-03-10 20:17:11 +0800

    Update SymbolicLogicCalculator.java

--- CS12S3/Personal/SymbolicLogicCalculator.java
+++ CS12S3/Personal/SymbolicLogicCalculator.java
@@ -1,5 +1,5 @@
-import java.io.*;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class SymbolicLogicCalculator {
     public static LinkedHashMap<String, ArrayList<Boolean>> propositions = new LinkedHashMap<>();
@@ -9,6 +9,93 @@ public class SymbolicLogicCalculator {
     public static String CompoundPropositionLeft = "";
     public static String CompoundPropositionRight = "";
     public static String CompoundPropositionLC = "";
+    public static Integer NegateCompoundProposition = 0;
+    
+    public static void removeDuplicates() {
+        LinkedHashMap<String, ArrayList<Boolean>> temp = new LinkedHashMap<>();
+        for (String p : propositions.keySet()) {
+            if (p.startsWith("(") && p.endsWith(")")) {
+                if (propositions.keySet().contains(p.substring(1, p.length()-1))) {
+                    temp.put(p.substring(1, p.length()-1), propositions.get(p.substring(1, p.length()-1)));
+                }
+            } else {
+                temp.put(p, propositions.get(p));
+            }
+        }
+        propositions = temp;
+    }
+    
+    public static boolean isValid(String input) {
+        int POcount = 0;
+        for (int y = 0; y < input.length(); y++) {
+            if (input.substring(y, y+1).equals("(")) POcount++;
+            else if (input.substring(y, y+1).equals(")")) POcount--;
+        }
+        return POcount == 0;
+    }
+    
+    public static boolean checkIfCompoundNegated(String input) {
+        int POcount = 0;
+        if (input.startsWith("~(")) {
+            POcount++;
+            for (int y = 2; y < input.length(); y++) {
+                if (input.substring(y, y+1).equals("(")) POcount++;
+                else if (input.substring(y, y+1).equals(")")) POcount--;
+                if (POcount == 0 && y != input.length()-1) return false;
+            }
+        }
+        // If input is a negated compound proposition such as ~(((sAp)->(qVr))V(rVw))
+        NegateCompoundProposition++;
+        return true;
+    }
+    
+    public static void isCompoundPropositionNegated(String input) {
+        int POcount = 0;
+        boolean done = false;
+        String Compounded = input;
+        
+        if (input.startsWith("~(")) {
+            if (checkIfCompoundNegated(Compounded)) {
+                Compounded = Compounded.substring(2, Compounded.length()-1);
+                isCompoundPropositionNegated(Compounded);
+                return;
+            }
+        }
+        
+        CompoundProposition = Compounded;
+        
+        for (int y = 0; y < input.length(); y++ ) {
+            if (done) break;
+            if (Compounded.substring(y, y+1).equals("(")) POcount++;
+            else if (Compounded.substring(y, y+1).equals(")")) POcount--;
+            for (String LC : logicalConnectives) {
+                int from = y;
+                if (POcount > 0 || LC.equals("~")) continue;
+                if (input.substring(y, y+1).equals(LC)) {
+                    CompoundPropositionLeft = input.substring(0, y);
+                    CompoundPropositionRight = input.substring(y+1, input.length());
+                    CompoundPropositionLC = input.substring(y, y+1);
+                    done = true;
+                    break;
+                }
+                if (input.substring(from).contains("->") || input.substring(from).contains("<->")) {
+                    if (input.substring(from, y+2).equals("->") || input.substring(from, y+2).equals("==")) {
+                        CompoundPropositionLeft = input.substring(0, y);
+                        CompoundPropositionRight = input.substring(y+2, input.length());
+                        CompoundPropositionLC = input.substring(y, y+2);
+                        done = true;
+                        break;
+                    } else if (input.substring(from, y+3).equals("<->")) {
+                        CompoundPropositionLeft = input.substring(0, y);
+                        CompoundPropositionRight = input.substring(y+3, input.length());
+                        CompoundPropositionLC = input.substring(y, y+3);
+                        done = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
     
     public static void getSimplePropositions(String input) {
         input = input.replaceAll("\\s+","");
@@ -16,15 +103,13 @@ public class SymbolicLogicCalculator {
         int POcount = 0;
         HashMap<Integer, Integer> tempProposition = new HashMap<>();
         
-        if (input.startsWith("(") && input.endsWith(")") && !input.equals(CompoundProposition)) {
-            input = input.substring(1, input.length()-1);
-        }
+        if (input.startsWith("(") && input.endsWith(")") && !input.equals(CompoundProposition)) input = input.substring(1, input.length()-1);
         if (input.length() == 2) return;
         
         for (int y = 0; y < input.length(); y++) {
             if (input.substring(y, y+1).equals("(")) {
                 POcount++;
-                if (input.substring(y-1, y).equals("~")) {
+                if (y > 0 && input.substring(y-1, y).equals("~")) {
                     tempProposition.put(POcount, y-1);
                 } else tempProposition.put(POcount, y);
             } else if (input.substring(y, y+1).equals(")")) {
@@ -44,37 +129,12 @@ public class SymbolicLogicCalculator {
                     }
                 } else {
                     proposition.add(input.substring(y, y+2));
-                    proposition.add(input.substring(y+1, y+4));
-                }
-            }
-            for (String LC : logicalConnectives) {
-                int from = y;
-                if (POcount != 0 || LC.equals("~") || (!CompoundPropositionLeft.equals("") || !CompoundPropositionRight.equals("") || !CompoundPropositionLC.equals(""))) continue;
-                else if (input.substring(y, y+1).equals(LC)) {
-                    CompoundPropositionLeft = input.substring(0, y);
-                    CompoundPropositionRight = input.substring(y+1, input.length());
-                    CompoundPropositionLC = input.substring(y, y+1);
-                }
-                if (input.contains("->") || input.contains("<->")) {
-                    if (input.substring(from, y+2).equals("->") || input.substring(from, y+2).equals("==")) {
-                        CompoundPropositionLeft = input.substring(0, y);
-                        CompoundPropositionRight = input.substring(y+2, input.length());
-                        CompoundPropositionLC = input.substring(y, y+2);
-                    } else if (input.substring(from, y+3).equals("<->")) {
-                        CompoundPropositionLeft = input.substring(0, y);
-                        CompoundPropositionRight = input.substring(y+3, input.length());
-                        CompoundPropositionLC = input.substring(y, y+3);
-                    }
+                    if (getVariablesOfProposition(input.substring(y+1, y+4)) != 1) proposition.add(input.substring(y+1, y+4));
                 }
             }
         }
         
         for (String p : proposition) {
-            
-            if (p.startsWith("(") && p.endsWith(")") && !p.equals(CompoundProposition)) {
-                p = p.substring(1, p.length()-1);
-            }
-            
             if (!propositions.keySet().contains(p)) {
                 propositions.put(p, solveTruthValues(p));
             }
@@ -83,12 +143,16 @@ public class SymbolicLogicCalculator {
     
     public static ArrayList<String> getLeftRightProposition(String input) {
         ArrayList<String> proposition = new ArrayList<>();
+        int x = 0;
+        int POcount = 0;
         
         for (int to = 0; to <input.length(); to++) {
             for (String LC : logicalConnectives) {
                 if (LC.equals("~")) continue;
                 if (input.substring(to, to+1).equals(LC)) {
-                    proposition.add(input.substring(to-1, to));
+                    if (to > 1 && variables.contains(input.charAt(to-1)) && input.substring(to-2,to-1).equals("~")) x +=1;
+                    else x = 0;
+                    proposition.add(input.substring(to-1-x, to));
                     proposition.add(input.substring(to+1, input.length()));
                     proposition.add(LC);
                     break;
@@ -96,20 +160,18 @@ public class SymbolicLogicCalculator {
             }
             if (input.contains("->") || input.contains("<->")) {
                 if (input.substring(to, to+2).equals("->")) {
-                    proposition.add(input.substring(to-1, to));
+                    proposition.add(input.substring(to-1-x, to));
                     proposition.add(input.substring(to+2, input.length()));
                     proposition.add("->");
                 }
                 if (input.substring(to, to+3).equals("<->")) {
-                    proposition.add(input.substring(to-1, to));
+                    proposition.add(input.substring(to-1-x, to));
                     proposition.add(input.substring(to+3, input.length()));
                     proposition.add("<->");
                 }
             }
             if (!proposition.isEmpty()) break;
         }
-//        System.out.println("Proposition: "+input);
-//        System.out.println("PropositionsArray: "+proposition);
         return proposition;
     }
     
@@ -140,12 +202,12 @@ public class SymbolicLogicCalculator {
         String leftProposition = "";
         String rightProposition = "";
         String logicalConnective = "";
-        if (proposition == CompoundProposition) {
+        if (proposition.equals(CompoundProposition)) {
+            isCompoundPropositionNegated(proposition);
             leftProposition = CompoundPropositionLeft;
             rightProposition = CompoundPropositionRight;
             logicalConnective = CompoundPropositionLC;
         }
-        
         
         if (proposition.startsWith("~") && proposition.length() == 2) {
             propositions.put(proposition, negation(propositions.get(proposition.substring(1))));
@@ -163,7 +225,7 @@ public class SymbolicLogicCalculator {
             return negation(x);
         }
         
-        if (proposition.startsWith("~(") && proposition.endsWith("))") && !proposition.equals(CompoundProposition)) {
+        if (proposition.startsWith("~(") && proposition.endsWith("))") && (!proposition.equals(CompoundProposition) || variables.size() == 1)) {
             ArrayList<Boolean> x = new ArrayList<>();
             if (propositions.get(proposition.substring(2, proposition.length())) != null) x = propositions.get(proposition.substring(2, proposition.length()-1));
             else x = solveTruthValues(proposition.substring(2, proposition.length()-1));
@@ -171,8 +233,8 @@ public class SymbolicLogicCalculator {
             return negation(x);
         }
         
-        if (proposition.startsWith("(")) { 
-           int right = proposition.indexOf(")");
+        if (proposition.startsWith("(") && !proposition.equals(CompoundProposition) && leftProposition.equals("") && rightProposition.equals("")) { 
+            int right = proposition.indexOf(")");
             leftProposition = proposition.substring(1, right);
             if (proposition.length() == 5) {
                 leftProposition = proposition.substring(1, 2);
@@ -203,7 +265,7 @@ public class SymbolicLogicCalculator {
                     if (rightProposition != "") break;
                 }
             } // (aVb)Ac
-        } else if (!proposition.startsWith("(") && !proposition.equals(CompoundProposition)) {
+        } else if (!proposition.startsWith("(") && !proposition.equals(CompoundProposition) && leftProposition.equals("") && rightProposition.equals("")) {
             leftProposition = getLeftRightProposition(proposition).get(0);
             rightProposition = getLeftRightProposition(proposition).get(1);
             logicalConnective = getLeftRightProposition(proposition).get(2);
@@ -268,10 +330,7 @@ public class SymbolicLogicCalculator {
                 break;
         }
         
-//        System.out.println(proposition + " = " + leftTruthValues + " " + logicalConnective + " " + rightTruthValues + " = " + TruthValues);
-        
         propositions.put(proposition, TruthValues);
-        
         return TruthValues;
     }
     
@@ -297,15 +356,16 @@ public class SymbolicLogicCalculator {
     }
     
     public static void displayTruthTable(int rows) {
-        String[][] truthTable = new String[rows][15];
+        String[][] truthTable = new String[rows][50];
         int col = 1;
         int row = 0;
+        String truthTableDisplay = "";
         while (row < rows) {
             truthTable[row][0] = "| ";
             col = 1;
             if (row == 0) {
                 for (String proposition : propositions.keySet()) {
-                    truthTable[row][col] = proposition + " | ";
+                    truthTable[row][col] = proposition + "  |  ";
                     col++;
                 }
             } else {
@@ -316,13 +376,13 @@ public class SymbolicLogicCalculator {
                     truthTable[row][col] = "";
                     while (space < spacing) {
                         if (col == 1) break;
-                        truthTable[row][col] += " ";
+                        truthTable[row][col] += "  ";
                         space++;
                     }
                     truthTable[row][col] += ((truthValue) ? 1 : 0);
                     space = 0 + ((proposition.length()%2==0) ? 1 : 0);
                     while (space < spacing) {
-                        truthTable[row][col] += " ";
+                        truthTable[row][col] += "  ";
                         space++;
                     }
                     truthTable[row][col] += "|";
@@ -332,36 +392,24 @@ public class SymbolicLogicCalculator {
             row++;
         }
         for (int x = 0; x < rows; x++) {
-            for (int y = 0; y < 15; y++) {
+            for (int y = 0; y < 50; y++) {
                 String truthTableRow = truthTable[x][y];
-                if (truthTableRow != null) System.out.print(truthTableRow);
+                if (truthTableRow != null) truthTableDisplay += truthTableRow; // System.out.print(truthTableRow);
             }
-            System.out.println();
+            truthTableDisplay += '\n';
         }
+        JOptionPane.showMessageDialog(null, truthTableDisplay);
     }
     
     public static void main(String[] args) {
-        BufferedReader dataIn = new BufferedReader( new InputStreamReader(System.in) );
-        String input = "";
+        String input = JOptionPane.showInputDialog("Enter Proposition: ");
         
-        int variableNumbers = 0;
-        int numberOfRows = 0;
-        
-        try {
-            System.out.print("Proposition: ");
-            input = dataIn.readLine();
-            for (char x = 'a'; x <= 'z'; x++) {
-                for (int y = 0; y < input.length(); y++) {
-                    if (input.charAt(y) == x) {
-                        variables.add(x);
-                    }
-                }
-            }
-            variableNumbers = variables.size();
-            numberOfRows = (int)Math.pow(2, variableNumbers);
-        } catch ( IOException e ) {
-            System.out.println("Error Occured!");
+        for (char x = 'a'; x <= 'z'; x++) {
+            for (int y = 0; y < input.length(); y++) if (input.charAt(y) == x) variables.add(x);
         }
+        
+        int variableNumbers = variables.size();
+        int numberOfRows = (int)Math.pow(2, variableNumbers);
 
         for (int vn = 0; vn < variableNumbers; vn++) {
             propositions.put(Character.toString(getVariableFromSet(vn)), initializeTruthTable(numberOfRows, variableNumbers, vn + 1));
@@ -373,9 +421,23 @@ public class SymbolicLogicCalculator {
         
         input = input.replaceAll("\\s+","");
         
-        CompoundProposition = input;
-        getSimplePropositions(input);
-        solveTruthValues(input);
-        displayTruthTable(numberOfRows+1);
+        if (isValid(input)) {
+            CompoundProposition = input;
+            isCompoundPropositionNegated(CompoundProposition);
+            getSimplePropositions(CompoundProposition);
+            solveTruthValues(CompoundProposition);
+            for (int x = 1; x <= NegateCompoundProposition; x++) {
+                String p = "~(".repeat(x-1)+CompoundProposition+")".repeat(x-1);
+                propositions.put("~(".repeat(x)+CompoundProposition+")".repeat(x), negation(propositions.get(p)));
+            }
+            removeDuplicates();
+            displayTruthTable(numberOfRows+1);
+//            System.out.println(input.replace("<->", "=").replace("->", "=>").replace("A", "and").replace("O", "xor"));
+//            for (Character variable : variables) System.out.print(variable+",");
+//            System.out.println();
+        } else {
+//            System.out.println("Invalid input!");
+            JOptionPane.showMessageDialog(null, "Invalid input!");
+        }
     }
 }
